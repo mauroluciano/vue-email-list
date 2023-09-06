91d9618167f83c0e9a6a9a68983d3ce0d0dab7b3
mauroluciano <mauroluciano911@gmail.com>
2023-09-06 15:42:41 +0200

    add axios

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,48 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      mailArray: [
+        {
+          mail: "mymail.1@gmail.com",
+        },
+        {
+          mail: "mymail.2@gmail.com",
+        },
+        {
+          mail: "mymail.3@gmail.com",
+        },
+        {
+          mail: "mymail.4@gmail.com",
+        },
+        {
+          mail: "mymail.5@gmail.com",
+        },
+        {
+          mail: "mymail.6@gmail.com",
+        },
+        {
+          mail: "mymail.7@gmail.com",
+        },
+        {
+          mail: "mymail.8@gmail.com",
+        },
+        {
+          mail: "mymail.9@gmail.com",
+        },
+        {
+          mail: "mymail.10@gmail.com",
+        },
+      ],
     };
+  },
+
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        const randomNumber = response.data.response;
+        this.mailArray.mail = randomNumber;
+        console.log(randomNumber);
+      });
   },
 }).mount("#app");
