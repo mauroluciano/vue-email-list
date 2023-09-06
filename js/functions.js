7650b684fab30a158c3e318daebd45ae0dffb1f3
mauroluciano <mauroluciano911@gmail.com>
2023-09-06 15:52:55 +0200

    fix mail

--- js/functions.js
+++ js/functions.js
@@ -3,38 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      mailArray: [
-        {
-          mail: "mymail.1@gmail.com",
-        },
-        {
-          mail: "mymail.2@gmail.com",
-        },
-        {
-          mail: "mymail.3@gmail.com",
-        },
-        {
-          mail: "mymail.4@gmail.com",
-        },
-        {
-          mail: "mymail.5@gmail.com",
-        },
-        {
-          mail: "mymail.6@gmail.com",
-        },
-        {
-          mail: "mymail.7@gmail.com",
-        },
-        {
-          mail: "mymail.8@gmail.com",
-        },
-        {
-          mail: "mymail.9@gmail.com",
-        },
-        {
-          mail: "mymail.10@gmail.com",
-        },
-      ],
+      mailArray: 0,
     };
   },
 
@@ -43,8 +12,7 @@ createApp({
       .get("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((response) => {
         const randomNumber = response.data.response;
-        this.mailArray.mail = randomNumber;
-        console.log(randomNumber);
+        this.mailArray = randomNumber;
       });
   },
 }).mount("#app");
